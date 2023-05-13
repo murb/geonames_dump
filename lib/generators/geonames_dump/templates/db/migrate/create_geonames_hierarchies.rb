bb7d5aab4ba3c5a1cf66dd6f2990500ac0ac302f
murb <git@murb.nl>
2023-05-13 15:09:09 +0200

    add version to hierarchy migration

--- lib/generators/geonames_dump/templates/db/migrate/create_geonames_hierarchies.rb
+++ lib/generators/geonames_dump/templates/db/migrate/create_geonames_hierarchies.rb
@@ -1,5 +1,5 @@
 #  parentId, childId, type
-class CreateGeonamesHierarchies < ActiveRecord::Migration
+class CreateGeonamesHierarchies < ActiveRecord::Migration[6.0]
   def change
     create_table :geonames_hierarchies do |t|
       t.integer :parentId
